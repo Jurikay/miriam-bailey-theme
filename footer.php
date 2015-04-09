bdf52286f6201d9ea8ba6dc3511621dea0d67391
Jurikay <jurek.baumann@gmail.com>
2015-04-09 21:20:00 +0200

    footer text enfernt

--- footer.php
+++ footer.php
@@ -12,26 +12,6 @@ $social_links = bailey_get_social_links();
 
 	<footer id="site-footer" class="site-footer" role="contentinfo">
 		<?php bailey_maybe_show_footer_widgets(); ?>
-
-		<?php // Footer text
-		if ( $footer_text ) : ?>
-		<div class="footer-text">
-			<?php echo bailey_sanitize_text( $footer_text ); ?>
-		</div>
-		<?php endif; ?>
-
-		<div class="site-info">
-			<a href="https://thethemefoundry.com/wordpress-themes/bailey">
-				<em class="theme-name">Bailey WordPress template</em>
-			</a>
-			<span class="theme-by"><?php _ex( 'by', 'attribution', 'bailey' ); ?></span>
-			<em class="theme-author">
-				<a title="The Theme Foundry <?php esc_attr_e( 'homepage', 'bailey' ); ?>" href="https://thethemefoundry.com/">
-					The Theme Foundry
-				</a>
-			</em>
-		</div>
-
 		<?php // Social profile links
 		if ( ! empty( $social_links ) ) : ?>
 		<ul class="footer-social-links">
